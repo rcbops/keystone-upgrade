72d4c334a4ca4ede355e107dbc57e60f77652d72
Justin Shepherd <jshepher@rackspace.com>
2011-11-21 12:20:45 -0600

    this file will run all of the keystone-manage * list commands

---
+++ keystone_commands.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#      type       : role, tenant, user, token, endpoint, endpointTemplates
+#      command    : add, list, disable, delete, grant, revoke
+
+CMD='keystone-manage'
+
+echo "***** TENANT LIST ****"
+${CMD} tenant list
+tenants=(`${CMD} tenant list | grep -v -e '^id' -e '^--' | awk '{print $2}'`)
+# for i in $(seq 0 $((${#tenants[@]} - 1))); do echo ${tenants[i]}; done
+echo ""
+
+echo "***** USER LIST ****"
+${CMD} user list
+users=(`${CMD} user list | grep -v -e '^id' -e '^--' | awk '{print $2}'`)
+# for i in $(seq 0 $((${#users[@]} - 1))); do echo ${users[i]}; done
+echo ""
+
+echo "***** ROLE LIST ****"
+${CMD} role list
+echo ""
+
+for i in $(seq 0 $((${#tenants[@]} - 1))); do
+  echo "    ** ROLE LIST - TENANT ${tenants[i]} **"
+  ${CMD} role list ${tenants[i]}; 
+  echo ""
+done
+echo ""
+
+echo "***** ENDPOINT_TEMPLATES LIST ****"
+${CMD} endpointTemplates list
+echo ""
+
+echo "***** TOKEN LIST *****"
+${CMD} token list
+echo ""
+
+echo "***** ENDPOINT LIST *****"
+${CMD} endpoint list
+echo "FUNCTION DOES NOT EXIST IN KEYSTONE"
+echo ""
+
+echo "***** CREDENTIALS LIST *****"
+${CMD} credentials list
+echo "FUNCTION DOES NOT EXIST IN KEYSTONE"
+echo ""
